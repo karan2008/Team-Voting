82fbfc070dfbfdb49de1d6acbaf749802212e012
karan2008 <73336534+karan2008@users.noreply.github.com>
2022-07-23 12:42:28 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,23 @@
 import firebase from 'firebase';
 
-// add SDK Firebase
-var firebaseConfig = {
-
+ 
+   const firebaseConfig = {
+  apiKey: "AIzaSyAc_Is5ad6vyThOM3_RAtS3QsEXgxjIQVI",
+  authDomain: "team-voting-app-9d53f.firebaseapp.com",
+  projectId: "team-voting-app-9d53f",
+  storageBucket: "team-voting-app-9d53f.appspot.com",
+  messagingSenderId: "314919606101",
+  appId: "1:314919606101:web:3b012569b37d90184781dc"
 };
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
 
+
+
+    
+  
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+} else {
+  firebase.app();
+}
 export default firebase.database();
